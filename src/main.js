a0ad6b048fc81d75e8633f17222b1c9893763648
DarylAndrian <darylandrian@gmail.com>
2026-04-25 04:27:26 +0800

    fix: favicon + PrimeVue config + ignore node_modules
    - Add favicon.svg (money bag emoji)
    - Fix PrimeVue import (use 'primevue' not 'primevue/config')
    - Add .gitignore to exclude node_modules
    - Keep existing math fixes + chart
    - Build passes (379 KB JS)

--- src/main.js
+++ src/main.js
@@ -1,21 +1,11 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import PrimeVue from 'primevue/config'
-import Aura from '@primeuix/themes/aura'
 import App from './App.vue'
 
 const pinia = createPinia()
 const app = createApp(App)
 
 app.use(pinia)
-app.use(PrimeVue, {
-  theme: {
-    preset: Aura,
-    options: {
-      prefix: 'p',
-      darkModeSelector: 'system',
-      cssLayer: false
-    }
-  }
-})
+app.use(PrimeVue)
 app.mount('#app')
